bd7b9823942b88b378564f1aa004590f1f459cab
AlexandrDyomin <66584220+AlexandrDyomin@users.noreply.github.com>
2023-07-26 02:04:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ async function handleRequest(req, res) {
     res.setHeader('Access-Control-Allow-Origin', `*`);
     // res.setHeader('Vary', 'Origin');
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    let url = new URL(`${req.headers.host}${req.url}`);
-    console.log(url);
+    // let url = new URL(`${req.headers.host}${req.url}`);
     // let pathname = url.pathname.match(/\/.*/)[0];
     // routes[pathname] ? routes[pathname](req, res) : routes.default(req, res);
     res.end('hello');
