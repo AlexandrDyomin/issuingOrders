eba1c6bb9dee3b4899f26fb7993020559e6987ce
AlexandrDyomin <66584220+AlexandrDyomin@users.noreply.github.com>
2023-07-26 02:06:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function handleRequest(req, res) {
     // let url = new URL(`${req.headers.host}${req.url}`);
     // let pathname = url.pathname.match(/\/.*/)[0];
     // routes[pathname] ? routes[pathname](req, res) : routes.default(req, res);
-    res.end('hello');
+    routes.default(req, res);
 }
 
 function handleConnection() {
