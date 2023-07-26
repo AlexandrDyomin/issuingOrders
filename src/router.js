42c2c96b5c260153f4069be3734e9be36d1bfc3c
AlexandrDyomin <alexandr.dyomin@yahoo.com>
2023-07-27 06:02:08 +0900

    change

--- src/router.js
+++ src/router.js
@@ -59,8 +59,8 @@ function makeHandlerEnd(res, dataFromClient) {
             for (let i = 0; i < deserializedData.length; i++) {
                 let emptyOrderCopy = Buffer.concat([emptyOrder]);
                 let order = await generateDocument(emptyOrderCopy, deserializedData[i]);
-                await fs.writeFile(path.resolve(__dirname, '../tmp', `order${i}.docx`), order);
-                let pathOutput = await convertWordFiles(path.resolve(__dirname, '../tmp', `order${i}.docx`), 'pdf', path.resolve(__dirname, '../tmp'));
+                await fs.writeFile(path.resolve(process.cwd(), 'tmp', `order${i}.docx`), order);
+                let pathOutput = await convertWordFiles(path.resolve(process.cwd(), 'tmp', `order${i}.docx`), 'pdf', path.resolve(process.cwd(), 'tmp'));
                 let pdfBuf = await fs.readFile(pathOutput);
                 await merger.add(pdfBuf);
             }
