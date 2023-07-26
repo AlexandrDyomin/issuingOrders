54eaf89cd648e2cf62ff16d920056889beb8edc5
AlexandrDyomin <alexandr.dyomin@yahoo.com>
2023-07-27 05:18:13 +0900

    change

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ async function handleRequest(req, res) {
     res.setHeader('Vary', 'Origin');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     let url = new URL(`${req.headers.host}${req.url}`);
-    let pathname = url.pathname.match(/\/.*/)[0];
+    // let pathname = url.pathname.match(/\/.*/)[0];
+    res.end(url)
     // routes[pathname] ? routes[pathname](req, res) : routes.default(req, res);
 }
 
