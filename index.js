df1332bee89210b9fb594f08b558a8ac17da0bb3
Дёмин Александр Петрович <Adyomin@elsi-group.ru>
2023-08-08 02:02:22 +0900

    create base data, refactoring

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ let { routes } = require('./src/router.js');
 const HOST = 'localhost';
 const PORT = process.env.PORT || 8000;
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('db.sqlite3',  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE);
+
+// db.serialize(() => {
+//     db.run("CREATE TABLE lorem (info TEXT)");
+
+//     const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+//     for (let i = 0; i < 10; i++) {
+//         stmt.run("Ipsum " + i);
+//     }
+//     stmt.finalize();
+
+//     db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+//         console.log(row.id + ": " + row.info);
+//     });
+// });
+
+// db.close();
+
 const server = http.createServer();
 server.on('request', handleRequest);
 server.listen(PORT, HOST, handleConnection);
